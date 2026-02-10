1fab355a8d1b06881217755553a8021e5f41612d
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-02-10 13:43:36 +0100

    UPDATE: Added top-level content structure and routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
 function App() {
-    return <> </>;
+    return (
+    <>
+        <Header/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<StartupPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/selector" element={<SelectorPage/>}/>
+                <Route path="/profile" element={<LoginPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    </>);
 }
 
 export default App;
