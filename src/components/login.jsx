4dbc24f019919c10509f7107c52deea64f3847e3
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-02-13 15:12:06 +0100

    UPDATE: Improved styling of login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -31,22 +31,41 @@ export default function Login(props) {
         navigate('/profile');
     };
 
+    const loginPageStyle = "bg-neutral-100 flex h-full items-center justify-center";
+    const formStyle = "max-w-5xl rounded-3xl border border-white/60 bg-green-100/70 shadow-[0_20px_80px_-30px_rgba(0,0,0,0.25)] backdrop-blur-xl";
+    const innerPaddingClass = 'flex flex-col gap-8 p-8 sm:p-10';
+    const headingStyle = 'text-2xl sm:text-3xl font-bold text-sky-800';
+    const inputWrapperStyle = 'mt-3 rounded-2xl border border-cyan-100 bg-white/70 px-4 py-4 shadow-sm text-xl sm:text-2xl';
+    const inputFieldStyle = "flex items-center justify-center rounded-md outline-none text-gray-800";
+    const buttonStyle = 'cursor-pointer rounded-2xl px-5 py-4 text-xl sm:text-2xl font-semibold ' +
+        'text-white shadow-lg transition bg-gradient-to-r from-cyan-600 via-teal-600 to-emerald-600 ' +
+        'hover:from-cyan-500 hover:via-teal-500 hover:to-emerald-500 active:scale-[0.99] ' +
+        'focus:outline-none focus-visible:ring-4 focus-visible:ring-cyan-300/70'
+
+
     return (
-        <div className="min-h-screen flex items-center justify-center bg-neutral-100">
-            <form onSubmit={handleSubmit} className="bg-emerald-600 p-6 rounded-lg shadow-md w-80">
-                <input
-                    type="text"
-                    placeholder="Enter your name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="w-full px-3 py-2 rounded-md mb-4 outline-none text-gray-800"
-                />
-                <button
-                    type="submit"
-                    className="w-full bg-slate-700 hover:bg-slate-800 text-white py-2 rounded-md transition"
-                >
-                    Start
-                </button>
+        <div className={loginPageStyle}>
+            <form onSubmit={handleSubmit} className={formStyle}>
+                <div className={innerPaddingClass}>
+
+                    <h3 className={headingStyle}>Login | Register</h3>
+
+                    <div className={inputWrapperStyle}>
+                        <input
+                            type="text"
+                            placeholder="Enter your name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className={inputFieldStyle}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className={buttonStyle}
+                    >
+                        Start
+                    </button>
+                </div>
             </form>
         </div>
     );
